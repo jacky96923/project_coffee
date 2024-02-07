ea2dd1194805160413487c3e44a3ed179a7f4a25
Tom <tom12771277@gmail.com>
2024-02-07 17:28:46 +0800

    2 Login Pages

--- coffee-app/src/App.tsx
+++ coffee-app/src/App.tsx
@@ -1,13 +1,21 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import ClientLoginPage from "./ClientLoginPage";
+import BussinessLoginPage from "./BussinessLoginPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <ClientLoginPage />
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          //clinet
+          <Route path="/client-login" element={<ClientLoginPage />} />
+          //business
+          <Route path="/business-login" element={<BussinessLoginPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ coffee-app/src/BussinessLoginPage.tsx
@@ -0,0 +1,43 @@
+import { FormEvent, useState } from "react";
+
+export default function BussinessLoginPage() {
+  const [usernameInput, setUsernameInput] = useState("");
+  const [passwordInput, setPasswordInput] = useState("");
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("username", usernameInput);
+    console.log("password", passwordInput);
+  };
+
+  return (
+    <>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <h1>Project Coffee</h1>
+          <h3>Bussiness Version</h3>
+          <input
+            type="text"
+            name=""
+            id=""
+            placeholder="Username"
+            onChange={(e) => {
+              setUsernameInput(e.target.value);
+            }}
+            value={usernameInput}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            onChange={(e) => {
+              setPasswordInput(e.target.value);
+            }}
+            value={passwordInput}
+          />
+          <button>Login</button>
+          
+          <button>Create a new account</button>
+        </form>
+      </div>
+    </>
+  );
+}
