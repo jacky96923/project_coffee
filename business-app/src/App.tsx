5bc9cfe299f747356b713f7b9aa53d3ca24a41a6
Jacky Lai <jacky96923@gmail.com>
2024-02-08 10:34:31 +0800

    jacky created business-app

--- business-app/src/App.tsx
+++ business-app/src/App.tsx
@@ -1,17 +1,15 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import logo from "./logo.svg";
 import "./App.css";
-import ClientLoginPage from "./ClientLoginPage";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import BussinessLoginPage from "./BussinessLoginPage";
 import ShopRegisterPage from "./ShopRegisterPage";
+
 function App() {
   return (
     <Router>
       <div className="App">
         <Routes>
-          //clinet
-          <Route path="/client-login" element={<ClientLoginPage />} />
-          //business
           <Route path="/business-login" element={<BussinessLoginPage />} />
           <Route path="/shop-register" element={<ShopRegisterPage />} />
         </Routes>
--- server/migrations/20240207090840_create-users.ts
+++ server/migrations/20240207090840_create-users.ts
@@ -1,10 +1,16 @@
 import type { Knex } from "knex";
 
-
 export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable("users", function (table) {
+    table.increments().primary();
+    table.string("email", 255).notNullable();
+    table.string("contact_no", 64).notNullable();
+    table.integer("reward_points");
+    table.string("login_name", 64).notNullable();
+    table.string("login_password", 64).notNullable();
+  });
 }
-
 
 export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTable("users");
 }
-
