95f205e0000ba85b5f914f3c62ec7a8856e16208
Kenneth Pang <pangkaho8@gmail.com>
2024-02-08 10:22:30 +0800

    login v3

--- coffee-app/src/App.tsx
+++ coffee-app/src/App.tsx
@@ -4,19 +4,22 @@ import "./App.css";
 import ClientLoginPage from "./ClientLoginPage";
 import BussinessLoginPage from "./BussinessLoginPage";
 import ShopRegisterPage from "./ShopRegisterPage";
+import ClientRegisterPage from "./ClientRegisterPage";
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          //clinet
-          <Route path="/client-login" element={<ClientLoginPage />} />
-          //business
-          <Route path="/business-login" element={<BussinessLoginPage />} />
-          <Route path="/shop-register" element={<ShopRegisterPage />} />
-        </Routes>
-      </div>
-    </Router>
+    // <Router>
+    <div className="App">
+      <ClientRegisterPage />
+      {/* <Routes> */}
+      {/* //clinet */}
+      {/* <Route path="/" element={<ClientLoginPage />} /> */}
+      {/* <Route path="/client-login" element={<ClientLoginPage />} /> */}
+      {/* //business */}
+      {/* <Route path="/business-login" element={<BussinessLoginPage />} />
+          <Route path="/shop-register" element={<ShopRegisterPage />} /> */}
+      {/* </Routes> */}
+    </div>
+    // </Router>
   );
 }
 
