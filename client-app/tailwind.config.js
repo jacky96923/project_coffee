fa4b0f73741fc413c6262a1c9669bb18ac9f6a5d
Kenneth Pang <pangkaho8@gmail.com>
2024-02-08 16:06:19 +0800

    login v4

---
+++ client-app/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/features/login/ClientLoginPage.tsx",
+    "./src/features/register/ClientRegisterPage.tsx",
+    "./src/features/main/ClientMainPage.tsx",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
