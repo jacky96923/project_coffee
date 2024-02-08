f19b60181417d5cf937005527c1d08c9a8b68716
Jacky Lai <jacky96923@gmail.com>
2024-02-08 11:47:13 +0800

    jacky user shop table changes

--- server/migrations/20240207092329_create-shops.ts
+++ server/migrations/20240207092329_create-shops.ts
@@ -4,14 +4,14 @@ export async function up(knex: Knex): Promise<void> {
   return knex.schema.createTable("shops", function (table) {
     table.increments().primary();
     table.string("contact_no", 60).notNullable();
-    table.string("area", 60).notNullable();
-    table.string("district", 60).notNullable();
-    table.string("address", 255).notNullable();
+    table.string("area", 60);
+    table.string("district", 60);
+    table.string("address", 255);
     table.string("description", 255);
     table.string("login_name", 60).notNullable();
     table.string("login_password", 60).notNullable();
-    table.float("latitude", 8, 6).notNullable;
-    table.float("longitude", 9, 6).notNullable;
+    table.float("latitude", 8, 6);
+    table.float("longitude", 9, 6);
   });
 }
 
