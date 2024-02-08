d71c618f44fcedb1390a9a37f78bb50e23595b5b
Jacky Lai <jacky96923@gmail.com>
2024-02-08 17:33:24 +0800

    jacky .env setting

--- server/utils/jwt.ts
+++ server/utils/jwt.ts
@@ -1,5 +1,10 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+if (!process.env.JWT_SECRET)
+  throw Error(".env 's SECRET is absent, please fill in");
 export default {
-  jwtSecret: "fatboyeatallday",
+  jwtSecret: process.env.JWT_SECRET,
   jwtSession: {
     session: false,
   },
