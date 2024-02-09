78ce759a1ccc9b958e82d4faa4d9354155712935
Tom <tom12771277@gmail.com>
2024-02-09 14:07:58 +0800

    Half business login

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,9 +1,11 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import "./App.css";
+
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import ClientLoginPage from "./features/login/ClientLoginPage";
 import ClientRegisterPage from "./features/register/ClientRegisterPage";
 import ClientMainPage from "./features/main/ClientMainPage";
+
 import { RootState } from "./store";
 import { useSelector } from "react-redux";
 
