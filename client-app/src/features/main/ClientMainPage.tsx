698dd472ee7bdd225f1da8e0acc065c2948c4484
Kenneth Pang <pangkaho8@gmail.com>
2024-02-09 10:33:58 +0800

    daily update

--- client-app/src/features/main/ClientMainPage.tsx
+++ client-app/src/features/main/ClientMainPage.tsx
@@ -7,8 +7,11 @@ export default function ClientMainPage() {
         <div className="relative mx-auto max-w-7xl px-4 sm:static sm:px-6 lg:px-8">
           <div className="sm:max-w-lg">
             <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-              Good Morning, username{" "}
+              早晨, username{" "}
             </h1>
+            <div>
+              <input type="text" placeholder="尋找咖啡店" />
+            </div>
           </div>
         </div>
       </div>
