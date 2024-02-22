2e2b8383702fe71632cdd353f8526c1391d4e548
Kenneth Pang <pangkaho8@gmail.com>
2024-02-22 19:38:39 +0800

    add category successed

--- business-app/src/store.ts
+++ business-app/src/store.ts
@@ -1,12 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slices/authSlice";
-import regReducer from "./slices/RegSlice";
+// import regReducer from "./slices/RegSlice";
 // import regReducer from "./slices/RegSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
-    reg: regReducer,
+    // reg: regReducer,
   },
 });
 
