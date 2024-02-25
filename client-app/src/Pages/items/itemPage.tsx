441403d1ad2e231fcd8751a7491e65b33cfaec3a
Jacky Lai <jacky96923@gmail.com>
2024-02-25 18:45:57 +0800

    fixed itempage css

--- client-app/src/Pages/items/itemPage.tsx
+++ client-app/src/Pages/items/itemPage.tsx
@@ -96,7 +96,9 @@ export default function ItemPage() {
   const [cupSize, setCupSize] = useState("小杯");
   const [selectedSize, setSelectedSize] = useState("小杯");
   const [selectedSizeId, setSelectedSizeId] = useState(id);
-  const [selectedOptions, setSelectedOptions] = useState<{optionListName: string, optionName: string}[]>([]);
+  const [selectedOptions, setSelectedOptions] = useState<
+    { optionListName: string; optionName: string }[]
+  >([]);
   const [price, setPrice] = useState(
     typeof items === "string"
       ? ""
@@ -113,23 +115,26 @@ export default function ItemPage() {
       );
     }
   }, [items, cupSize]);
-//
+  //
   const getSelectedOption = useSelector(
     (state: RootState) => state.itemPage.item.optionList
   );
   //console.log("getSelected default", getSelectedOption);
-  
+
   useEffect(() => {
-    let displayOption: {optionListName: string, optionName: string}[] = [];
+    let displayOption: { optionListName: string; optionName: string }[] = [];
     getSelectedOption.forEach((entry) => {
-      displayOption.push({optionListName: entry.optionListName, optionName: entry.option.option_name});
+      displayOption.push({
+        optionListName: entry.optionListName,
+        optionName: entry.option.option_name,
+      });
     });
     //console.log("displayOption", displayOption)
     setSelectedOptions(displayOption);
     //console.log("ItemPageOptions Selected", selectedOptions);
   }, [getSelectedOption]);
   //console.log("selected outside useEffect", selectedOptions)
-//
+  //
   function sizeHandler(size: string) {
     setSelectedSize(size);
     if (typeof items !== "string") {
@@ -179,113 +184,120 @@ export default function ItemPage() {
 
   return (
     <>
-      <div className={styles.imageContainer}>
-        <img
-          className={styles.itemImg}
-          src={typeof items === "string" ? "" : items.itemInfo[0].item_photo}
-        ></img>
-      </div>
-      <button
-        onClick={() => navigate(-1)}
-        className={`btn btn-circle btn-sm ${styles.backButton}`}
-      >
-        <ChevronLeftIcon className="h-5 w-5 text-white" />
-      </button>
-      <div className={styles.content}>
-        <div className={styles.itemWrap}>
-          <div className={styles.itemName_container}>
-            <div className={styles.itemName}>
-              {typeof items === "string" ? "No data" : items.itemInfo[0].name}
-            </div>
-          </div>
-          <div className={styles.itemCard}>
-            <div className={styles.itemDetails}>
-              <div className={styles.itemDescription}>
-                <div className={styles.itemh2}>關於此產品</div>
-                <hr className={styles.divider} />
-                <div className={styles.itemh3}>
-                  {typeof items === "string"
-                    ? "No data"
-                    : items.itemInfo[0].description}
-                </div>
+      <div className="-mt-10">
+        <div className={styles.imageContainer}>
+          <img
+            className={styles.itemImg}
+            src={typeof items === "string" ? "" : items.itemInfo[0].item_photo}
+          ></img>
+        </div>
+        <button
+          onClick={() => navigate(-1)}
+          className={`btn btn-circle btn-sm ${styles.backButton}`}
+        >
+          <ChevronLeftIcon className="h-5 w-5 text-white" />
+        </button>
+        <div className={styles.content}>
+          <div className={styles.itemWrap}>
+            <div className={styles.itemName_container}>
+              <div className={styles.itemName}>
+                {typeof items === "string" ? "" : items.itemInfo[0].name}
               </div>
-              <div className={styles.itemDescription}>
-                {typeof items === "string" ? (
-                  ""
-                ) : items.itemInfo.length > 1 ? (
-                  <div>
-                    <div className={styles.itemh2}>飲品尺寸</div>
-                    <div className={styles.sizeButtonWrap}>
-                      {typeof items === "string"
-                        ? " "
-                        : items.itemInfo.map((entry) => (
-                            <button
-                              onClick={() => sizeHandler(entry.size)}
-                              className={`btn btn-warning bg-gradient-to-r from-light-brown to-dark-brown${
-                                selectedSize === entry.size
-                                  ? styles.selectedSize
-                                  : ""
-                              }`}
-                            >
-                              <div className={styles.buttonText}>
-                                {entry.size}
-                              </div>
-                            </button>
-                          ))}
-                    </div>
+            </div>
+            <div className={styles.itemCard}>
+              <div className={styles.itemDetails}>
+                <div className={styles.itemDescription}>
+                  <div className={styles.itemh2}>關於此產品</div>
+                  <hr className={styles.divider} />
+                  <div className={styles.itemh3}>
+                    {typeof items === "string"
+                      ? ""
+                      : items.itemInfo[0].description}
                   </div>
-                ) : (
-                  " "
-                )}
+                </div>
+                <div className={styles.itemDescription}>
+                  {typeof items === "string" ? (
+                    ""
+                  ) : items.itemInfo.length > 1 ? (
+                    <div>
+                      <div className={styles.itemh2}>飲品尺寸</div>
+                      <div className={styles.sizeButtonWrap}>
+                        {typeof items === "string"
+                          ? " "
+                          : items.itemInfo.map((entry) => (
+                              <button
+                                onClick={() => sizeHandler(entry.size)}
+                                className={`btn btn-lg btn-warning bg-gradient-to-r from-light-brown to-dark-brown${
+                                  selectedSize === entry.size
+                                    ? styles.selectedSize
+                                    : ""
+                                }`}
+                              >
+                                <div className={styles.buttonText}>
+                                  {entry.size}
+                                </div>
+                              </button>
+                            ))}
+                      </div>
+                    </div>
+                  ) : (
+                    " "
+                  )}
+                </div>
               </div>
-            </div>
-            <div className={styles.optionsWrap}>
-              <div className={styles.optionsTitle}>
-                <div className={styles.itemh2}>自訂選項</div>
-              </div>
+              <div className={styles.optionsWrap}>
+                <div className={styles.optionsTitle}>
+                  <div className={styles.itemh2}>自訂選項</div>
+                </div>
 
-              {typeof items === "string"
-                ? ""
-                : items.optionList.length > 0
-                ? items.optionList.map((entry, index) => (
-                    <ItemPageOptions
-                      key={index}
-                      optionListName={entry.option_list_name}
-                      selectedOption={selectedOptions.find((option)=>option.optionListName===entry.option_list_name)?.optionName || ""}
-                      itemId={selectedSizeId!}
-                      // Pass the selected option at the same index
-                    />
-                  ))
-                : "No Option data"}
-            </div>
-            <div className={styles.addItemWrap}>
-              <div className={styles.quantityWrap}>
-                <button
-                  className="btn btn-circle bg-slate-500 w-8 h-8"
-                  onClick={decreaseQuantity}
-                >
-                  <MinusIcon className="fill-white w-6 h-5" />
-                </button>
-                <div className={styles.count}>
-                  <div className="text-white">{quantity}</div>
+                {typeof items === "string"
+                  ? ""
+                  : items.optionList.length > 0
+                  ? items.optionList.map((entry, index) => (
+                      <ItemPageOptions
+                        key={index}
+                        optionListName={entry.option_list_name}
+                        selectedOption={
+                          selectedOptions.find(
+                            (option) =>
+                              option.optionListName === entry.option_list_name
+                          )?.optionName || ""
+                        }
+                        itemId={selectedSizeId!}
+                        // Pass the selected option at the same index
+                      />
+                    ))
+                  : "No Option data"}
+              </div>
+              <div className={styles.addItemWrap}>
+                <div className={styles.quantityWrap}>
+                  <button
+                    className="btn btn-circle bg-slate-500 w-8 h-8"
+                    onClick={decreaseQuantity}
+                  >
+                    <MinusIcon className="fill-white w-6 h-5" />
+                  </button>
+                  <div className={styles.count}>
+                    <div className="text-white">{quantity}</div>
+                  </div>
+                  <button
+                    className="btn btn-circle bg-red-600 w-8 h-8"
+                    onClick={increaseQuantity}
+                  >
+                    <PlusIcon className="fill-white w-6 h-5" />
+                  </button>
                 </div>
                 <button
-                  className="btn btn-circle bg-red-600 w-8 h-8"
-                  onClick={increaseQuantity}
+                  className="btn rounded-full bg-yellow-900 px-28"
+                  onClick={handleAddToCart}
                 >
-                  <PlusIcon className="fill-white w-6 h-5" />
+                  <div className={styles.addItemContent}>
+                    <div className="text-white">加入購物車</div>
+                    <div className={styles.vl}></div>
+                    <div className="text-white">${subtotal}</div>
+                  </div>
                 </button>
               </div>
-              <button
-                className="btn rounded-full bg-yellow-900 px-28"
-                onClick={handleAddToCart}
-              >
-                <div className={styles.addItemContent}>
-                  <div className="text-white">加入購物車</div>
-                  <div className={styles.vl}></div>
-                  <div className="text-white">${subtotal}</div>
-                </div>
-              </button>
             </div>
           </div>
         </div>
