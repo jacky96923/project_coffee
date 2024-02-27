06c5b91b1498bf96c801f3575e854fcd2eb58faf
Tom <tom12771277@gmail.com>
2024-02-28 01:01:37 +0800

    Corrected API fetching to Business Comment

--- server/controller/CommentsController.ts
+++ server/controller/CommentsController.ts
@@ -11,13 +11,15 @@ export class CommentsController {
 
   private initializeRoutes() {
     this.router.post('/rating', this.createComment.bind(this));
+    this.router.get('/all', this.getAllComments.bind(this));
+
+    // New route
   }
 
   private async createComment(req: express.Request, res: express.Response) {
     const { shopId, transactionId, rating, description } = req.body;
     console.log("Rating:", rating);
     try {
-      // Call the createComment method on the commentService
       const result = await this.commentService.createComment(shopId, transactionId, rating, description);
       res.status(201).json(result);
     } catch (error) {
@@ -25,4 +27,16 @@ export class CommentsController {
       res.status(500).json({ error: "Internal server error" });
     }
   }
-}  
+
+  private async getAllComments(req: express.Request, res: express.Response) {
+    try {
+      const comments = await this.commentService.getAllComments("description");
+      res.status(200).json(comments);
+    } catch (error) {
+      console.error("Error fetching all comments:", error);
+      res.status(500).json({ error: "Internal server error" });
+    }
+  }
+  
+  }
+
