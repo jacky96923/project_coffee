01ca7727982a5bbfa8023ea0d98ae2da5531f3ab
Jacky Lai <jacky96923@gmail.com>
2024-02-29 16:02:33 +0800

    changed seed data

--- server/migrations/20240208033355_create-shop_photos.ts
+++ server/migrations/20240208033355_create-shop_photos.ts
@@ -3,7 +3,7 @@ import type { Knex } from "knex";
 export async function up(knex: Knex): Promise<void> {
   return knex.schema.createTable("shop_photos", function (table) {
     table.increments().primary();
-    table.string("filename", 255);
+    table.string("filename", 1000);
     table.boolean("cover_photo");
     table.boolean("logo");
     table.integer("shop_id").unsigned().references("id").inTable("shops");
